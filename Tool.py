d5c37ff96cdd888aa55f84dc472a532e94920d4f
Daniël Klouwens <dklouwens@tudelft.nl>
2024-01-18 22:30:36 +0100

    V2.0

--- Tool.py
+++ Tool.py
@@ -7,42 +7,49 @@ import numpy as np
 
 #Spacecraft Parameters
 
-Ps   =                       #[W] Transmitter Power
-Ds   =                       #[m] Spacecraft Antenna Diameter
-Tns  =                      #[K] Noise Temperature
-wl =                       #[m] Wavelength
-f   =                       #[GHz] Frequency
-POAs =                       #[deg] Pointing Offset Angle
+Ps   = 10                      #[W] Transmitter Power
+Ds   = 10                      #[m] Spacecraft Antenna Diameter
+wl =   10                    #[m] Wavelength
+f   =  10                     #[GHz] Frequency
+POAs = 10                      #[deg] Pointing Offset Angle
 
-SWA =                       #[deg] Swath Width Angle
-PSA =                       #[arcmin] Pixel Size Angle
-Bp  =                       #[-] Bits per Pixel
-Dc  =                       #[%] Duty Cycle
-Tdl =                       #[hr/day] Downlink Time
+SWA =   10                    #[deg] Swath Width Angle
+PSA =   10                  #[arcmin] Pixel Size Angle
+Bp  =   10                  #[-] Bits per Pixel
+Dc  =   10                 #[%] Duty Cycle
+Tdl =   10                     #[hr/day] Downlink Time
 
-InterPlanetaty = True
-hb =                       #[m] Orbit Altitude above target body
-Mb  =                       #[kg] Target Body Mass
-Rb  =                       #[m] Target Body Radius
 
-he =                       #[m] Orbit Altitude above Earth
-
-e  =                       #[deg] Elongation Angle
-dse =                       #[m] Distance between Earth and Sun
-dss =                       #[m] Distance between Sun and Spacecraft
 
 #Ground Station Parameters
-Pg =                       #[W] Transmitter Power
-Dg =                       #[m] Ground Antenna Diameter
-Tng =                       #[K] Noise Temperature
-POAg =                       #[deg] Pointing Offset Angle
-Ru =                       #[bit/s] Required Uplink Data Rate
+Pg =  10                     #[W] Transmitter Power
+Dg =  10                     #[m] Ground Antenna Diameter
+
+POAg = 10                      #[deg] Pointing Offset Angle
+Ru =   10                    #[bit/s] Required Uplink Data Rate
 
 
-LFt =                       #[-] Transmitter Loss Factor
-LFr =                       #[-] Receiver Loss Factor
-TAR =                       #[-] Turn around Ratio
+LFt =    10                  #[-] Transmitter Loss Factor
+LFr =    10                   #[-] Receiver Loss Factor
+TAR =    10                   #[-] Turn around Ratio
 eff =0.55                   #[-] Antenna Efficiency (0.55 for parabolic))
+
+#Target Body Parameters
+hb =     10                    #[m] Orbit Altitude above target body
+Mb  =    10                   #[kg] Target Body Mass
+Rb  =    10                   #[m] Target Body Radius
+
+InterPlanetaty = True
+#if false fill this in:
+he =     10                 #[m] Orbit Altitude above Earth
+
+#if true fill this in:
+e  =     10                  #[deg] Elongation Angle
+dse =    10                   #[m] Distance between Earth and Sun
+dss =    10                   #[m] Distance between Sun and Spacecraft
+
+Tn_d  =   10                   #[K] Noise Temperature Dowlink
+Tn_u =    10                   #[K] Noise Temperature Uplink
 
 #Other Parameters
 Re  = 6371000                   #[m] Earth Radius
@@ -52,7 +59,7 @@ kb = 1.38065*(10**-23)         #[m^2 kg s^-2 K^-1] Boltzmann Constant
 
 #Caluclations:
 def to_dB(x): #Converts input to dB
-    return 10 * np.log10(x)
+    return 10 * np.log10(np.abs(x))
 
 #Transmitter Power
 Ps = to_dB(Ps) #dB
@@ -107,12 +114,36 @@ Rd = Dc / Tdl
 def EIPR(P, D):
     return to_dB(P) + to_dB(G(D)) -to_dB(LFt)
 
+def get_Tn_d(f):
+    if f == 0.2:
+        return 221
+    if f <= 12:
+        if f >= 2:
+            return 135
+    if f == 20:
+        return 424
+    else:
+      print("Frequency out of range, enter Tn_d manually")
+      return Tn_d
+        
+def get_Tn_u(f):
+    if f <= 20:
+        if f >= 0.2:
+            return 614
+    if f == 40:
+        return 763
+    else:
+        print("Frequency out of range, enter Tn_u manually")
+        return Tn_u
+
 
     
 def SNR(P, Tn, Dt, Dr, POAt):
     return EIPR(P, Dt) - to_dB(Ls) - to_dB(Lpr(Dt, POAt)) - to_dB(LFr) + to_dB(G(Dr)) - to_dB(Rd) - to_dB(TAR) - to_dB(Tn * kb) 
 
-print ("SNR Downlink: ", SNR(Ps, Tns, Ds, Dg, POAs))
-print ("SNR Uplink: ", SNR(Pg, Tng, Dg, Ds, POAg))
+print ("SNR Downlink: ", SNR(Ps, get_Tn_d(f), Ds, Dg, POAs))
+print ("SNR Uplink: ", SNR(Pg, get_Tn_u(f), Dg, Ds, POAg))
+
+
 
 
